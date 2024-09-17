59eb8c271ff3993f8db9a25253cb41270e26819f
Linda-Moreno <cristinamachadotapia@gmail.com>
2024-09-17 14:26:51 -0500

    se crea servidor

---
+++ src/main/java/com/example/servidor_suratech4/MODELO/Paciente.java
@@ -0,0 +1,16 @@
+package com.example.servidor_suratech4.MODELO;
+
+import java.time.LocalDate;
+
+public class Paciente {
+    Integer id;
+    String nombres;
+    Integer edad;
+    LocalDate fechaNacimiento;
+    String documento;
+    String telefono;
+    Integer copago;
+    String email;
+    String direccion;
+    String medicoFamilia;
+}
